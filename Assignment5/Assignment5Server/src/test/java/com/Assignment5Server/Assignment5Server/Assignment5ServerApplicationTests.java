3999974850906db82570785dd0cc7e5cf20b72e7
Frank Ziegler <ziegle15@students.rowan.edu>
2025-04-16 17:27:18 -0400

    assignment 5 init with jackson dependencies

---
+++ Assignment5/Assignment5Server/src/test/java/com/Assignment5Server/Assignment5Server/Assignment5ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assignment5Server.Assignment5Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment5ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
